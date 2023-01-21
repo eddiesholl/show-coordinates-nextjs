f6a2f07f2c847108988ba30bc2779d9e0f924834
Eddie Sholl <eddie.sholl@pathzero.com>
2023-01-21 12:13:03 +1100

    feat: @turf/bbox to render map with points in view

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,12 @@
 import MapWrapper, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import bbox from "@turf/bbox";
 
 const token =
   "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
 
 const locations = {
+  type: "FeatureCollection",
   features: [
     {
       type: "Feature",
@@ -17,21 +19,43 @@ const locations = {
         type: "Point",
       },
     },
+    {
+      type: "Feature",
+      properties: {
+        title: "Somewhere else",
+        description: "A northside park that is home to the Lincoln Park Zoo",
+      },
+      geometry: {
+        coordinates: [-88.637596, 42.940403],
+        type: "Point",
+      },
+    },
   ],
 };
 const Map = () => {
+  const [minLong, minLat, maxLong, maxLat] = bbox(locations);
+
+  const markers = locations.features.map((f, ix) => (
+    <Marker
+      key={ix}
+      longitude={f.geometry.coordinates[0]}
+      latitude={f.geometry.coordinates[1]}
+      color="red"
+    />
+  ));
   return (
     <MapWrapper
       initialViewState={{
-        longitude: -100,
-        latitude: 40,
-        zoom: 3.5,
+        bounds: [minLong, minLat, maxLong, maxLat],
+        fitBoundsOptions: {
+          padding: 40,
+        },
       }}
       style={{ width: 600, height: 400 }}
       mapStyle="mapbox://styles/mapbox/streets-v12"
       mapboxAccessToken={token}
     >
-      <Marker longitude={-122.4} latitude={37.8} color="red" />
+      {markers}
     </MapWrapper>
   );
 };
